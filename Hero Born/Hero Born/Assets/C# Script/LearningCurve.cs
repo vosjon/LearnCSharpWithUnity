50a1d6b9bf8af37945fe28faf61b94d994cc7edd
Jonathan Vos <vosjonjoe@gmail.com>
2023-12-06 18:08:11 -0800

    Completed Chapter 2. Created a method in the LearningCurve script.

--- Hero Born/Hero Born/Assets/C# Script/LearningCurve.cs
+++ Hero Born/Hero Born/Assets/C# Script/LearningCurve.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class LearningCurve : MonoBehaviour
 {
+    public int currentAge = 30;
+    public int addedAge = 1;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log(30 + 1);
+        Debug.Log(currentAge + 1);
+        ComputeAge();
+    }
+    /// <summary>
+    /// Computes a modified age integer
+    /// </summary>
+    void ComputeAge()
+    {
+        Debug.Log(currentAge +  addedAge);
     }
 
     // Update is called once per frame
